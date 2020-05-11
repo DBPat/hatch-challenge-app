1323a2ce9d65383d5582b1be4c8eda5c872aa49c
Dharmesh Patel <d.patel5603@gmail.com>
2020-05-11 03:37:27 -0700

    set p typescript, tslint and initial components

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import {Container} from './NavBar.styles'
+
+export default function NavBar() {
+  return (
+    <Container>
+      Nav Bar
+    </Container>
+  )
+}
---
+++ src/components/UserTable/UserTable.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import {Container} from './UserTable.styles'
+
+export default function UserTable() {
+  return (
+    <Container>
+      User Table
+    </Container>
+  )
+}
